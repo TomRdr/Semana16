e933cba949c916d672a2c68b870ef0077b5ddd8a
TomRdr <72907542+TomRdr@users.noreply.github.com>
2021-05-09 23:59:28 -0600

    Update 2.js

--- Tarea/2.js
+++ Tarea/2.js
@@ -1,42 +1,35 @@
 const interface = require(`readline-sync`);
-
-//Suma, Resta, Multiplicación y division
 let response = "";
-
 do{
-    
     console.log("1.- Suma");
     console.log("2.- Resta");
-    console.log("3.- Multiplicacion");
-    console.log("4.- Division");
-    console.log("5.- Salir");
-    response = interface.question("Que operacion deseas hacer?:")
-
+    console.log("3.- Mult");
+    console.log("4.- Div");
+    console.log("5.-Salir")
+    response = interface.question("Selecione la operacion")
     if(response != "5"){
-        const Numero1 = interface.question("Indique el primer valor: ");
-        const Numero2 = interface.question("Indique el segundo valor: ");
-
+        const num1 = interface.question("primer numero");
+        const num2 = interface.question("segundo numero");
         switch(response){
             case "1":
-                var result = parseInt(Numero1) + parseInt(Numero2);
+                var result = parseInt(num1) + parseInt(num2);
                 console.log("Respuesta: "+result);
                 break;
             case "2":
-                    var result = parseInt(Numero1) - parseInt(Numero2);
+                    var result = parseInt(num1) - parseInt(num2);
                     console.log("Respuesta: "+result);
                     break;
             case "3":
-                    var result = parseInt(Numero1) * parseInt(Numero2);
+                    var result = parseInt(num1) * parseInt(num2);
                     console.log("Respuesta: "+result);
                     break;
             case "4":
-                    var result = parseInt(Numero1) / parseInt(Numero2);
+                    var result = parseInt(num1) / parseInt(num2);
                     console.log("Respuesta: "+result);
                     break;
                 default:
                     break;
         }
-
-        interface.question("Presiona Enter para continuar");
+        interface.question("enter para poder continuar");
     }
 }while(response != "5");
